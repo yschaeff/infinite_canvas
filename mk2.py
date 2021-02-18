a199646f3443dae72f7a31d61c31a05896745049
Yuri Schaeffer <yuri@schaeffer.tk>
2021-02-18 09:40:57 +0100

    Draw sketches, blit to current frame.

--- mk2.py
+++ mk2.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
 import time, pickle
 import tkinter as tk
 from functools import partial
 
 PICKLE_FILE = "canvas.pickle"
+
+def pairs(l, loop=False):
+    if not l: return
+    first = l[0]
+    for i in l[1:]:
+        second = i
+        yield (first, second)
+        first = second
+    if loop:
+        yield (first, l[0])
 
 class Viewport:
     def __init__(self, viewport=None):
@@ -33,7 +44,8 @@ class Stroke:
         self.color = color
         self.style = style
     def render(self, canvas, vieport):
-        pass
+        for p1, p2 in pairs(self.path, loop=False):
+            obj_id = canvas.create_line(*p1, *p2, fill=self.color, width=3)
 
 class Frame:
     def __init__(self, viewport):
@@ -41,6 +53,7 @@ class Frame:
         self.create_time = time.time()
         self.modify_time = self.create_time
         self.drawables = []
+        print("new frame")
     def render(self, canvas, current_viewport):
         vp_diff = self.viewport.subtract(current_viewport)
         for drawable in self.drawables:
@@ -52,27 +65,74 @@ class Data:
         print("created")
         self.initial_viewport = Viewport()
         self.frames = []
+        self.frame_lru = []
     def initialize(self):
         """exec always"""
         print("init")
         self.resolution = 500
         self.current_viewport = self.initial_viewport
+    def push_sketch(self, sketch):
+        if not self.frame_lru or not self.frame_lru[0].viewport is self.current_viewport:
+            frame = Frame(self.current_viewport)
+            self.frames.append(frame)
+            self.frame_lru.append(frame)
+        else:
+            frame = self.frame_lru[-1]
+        sketch.blit(frame)
     def render(self, canvas):
         for frame in self.frames:
-            frame.render(canvas, self.viewport)
+            frame.render(canvas, self.current_viewport)
+
+class Sketch:
+    def __init__(self):
+        self.stroke = Stroke([], "#BCB534", None)
+    def push(self, x, y):
+        self.stroke.path.append( (x,y) )
+    def blit(self, frame):
+        print(f"Adding stroke {id(self.stroke)} to frame {id(frame)}")
+        frame.drawables.append(self.stroke)
+        self.stroke = Stroke([], "#BCB534", None)
+    def render(self, canvas):
+        for p1, p2 in pairs(self.stroke.path, loop=False):
+            obj_id = canvas.create_line(*p1, *p2, fill="#BC347F", width=12)
 
 def quit(event, context):
     context.root.quit()
+
+def start_draw(event, context):
+    x, y = event.x, event.y
+    context.sketch.push(x, y)
+    context.redraw()
+def continue_draw(event, context):
+    x, y = event.x, event.y
+    context.sketch.push(x, y)
+    context.redraw()
+def stop_draw(event, context):
+    x, y = event.x, event.y
+    context.sketch.push(x, y)
+    context.data.push_sketch(context.sketch)
+    context.redraw()
 
 class Context:
     def __init__(self, data):
         self.root = tk.Tk()
         self.canvas = tk.Canvas(self.root)
         self.data = data
+        self.sketch = Sketch()
+    def redraw(self):
+        self.canvas.delete("all")
+        self.sketch.render(self.canvas)
+        self.data.render(self.canvas)
 
 def init_gui(context):
     context.canvas.pack(expand = True, fill = tk.BOTH)
     context.canvas.bind_all("<Key-q>", partial(quit, context=context))
+    context.canvas.bind("<Button-1>", partial(start_draw, context=context))
+    context.canvas.bind("<B1-Motion>", partial(continue_draw, context=context))
+    context.canvas.bind("<ButtonRelease-1>", partial(stop_draw, context=context))
+    #contect.canvas.bind("<Button-3>", partial(start_move, context=context))
+    #contect.canvas.bind("<B3-Motion>", partial(continue_move, context=context))
+    #contect.canvas.bind("<ButtonRelease-3>", partial(stop_move, context=context))
 
 def main():
     def load(fn):
